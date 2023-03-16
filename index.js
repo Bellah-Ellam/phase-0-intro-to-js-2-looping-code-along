e99f7ac5fd646b208d4e74aec01ab02cc32baaed
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-03-16 09:38:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const gifters=["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+    const cards = []; // initialize an empty array to store the result
+  
+    for (let i = 0; i < names.length; i++) {
+      cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return cards; // return the array of cards
+  }
+  
+writeCards(gifters,'surprise');
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
